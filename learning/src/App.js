fe325258e4ffe67f94c39e504e4eea63c19c04ae
prajwal6394 <prajwalpandey2009@gmail.com>
2022-03-01 01:57:46 +0530

    Revison.target.success

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import { useState } from 'react'
+import Hello from './components/Hello';
+
+const App = () => {
+  const [set, setcount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Rect
+      </h1>
+      <p>Click Value has been change {set} times</p>
+      <button onClick={() => setcount(set+1)}>Click</button>
+      <Hello name = "Prajwal"power = "Developer" />
+      <Hello name = "Rishab" power = "Player"/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
