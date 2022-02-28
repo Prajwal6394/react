3bd4a397ad3bcc8c81edebe24305970e7b6fbae7
prajwal6394 <prajwalpandey2009@gmail.com>
2022-03-01 02:29:25 +0530

    Goodnight

--- learning/src/components/Hello.js
+++ learning/src/components/Hello.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Hello = (props) => {
+  const style = {
+    color: 'black',
+    fontSize: 100,
+    textAlign: 'center'
+  };
   return (
     <div>
-        <h1>Hello {props.name} {props.Greet}</h1>
+        <h1 style={style} >Hello {props.name} {props.Greet}</h1>
     </div>
   )
 }
